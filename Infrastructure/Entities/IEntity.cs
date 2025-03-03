39031da49563d5d6e13c6f53a722b0f8fd1edbf3
hector <hector26rubio@gmail.com>
2025-03-02 23:47:00 -0500

    add  entity generic

---
+++ Infrastructure/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Transport.Infrastructure.Entities
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ Infrastructure/Entities/Impl/Entity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Transport.Infrastructure.Entities.Impl
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Entity : IEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+}
